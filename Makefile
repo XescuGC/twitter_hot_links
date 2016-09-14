5c6e05540dfed32e364fab805158a6286e026d06
diegok <diego@freekeylabs.com>
2016-09-14 02:20:26 +0200

    Better config and initial Makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+build:
+	go build -o hot-links -v *.go
+
+run:
+	go run *.go
+
+
--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 )
@@ -13,18 +14,32 @@ type Config struct {
 	AccessSecret   string `json:"access_secret"`
 }
 
+type Opts struct {
+	configFile string
+}
+
 func readConfig() Config {
 	var c Config
 
-	configFile, err := os.Open("config.json")
+	getopts := getOpts()
+
+	configFile, err := os.Open(getopts.configFile)
 	if err != nil {
-		fmt.Println("Opening config file", err.Error())
+		fmt.Println("\x1b[31m[Config file]\x1b[39m", err.Error())
+		os.Exit(1)
 	}
 
 	jsonParser := json.NewDecoder(configFile)
 	if err = jsonParser.Decode(&c); err != nil {
-		fmt.Println("Parsing config file", err.Error())
+		fmt.Println("\x1b[31m", "Parsing config file error: ", err.Error(), "\x1b[39m")
+		os.Exit(1)
 	}
 
 	return c
+}
+
+func getOpts() Opts {
+	configFile := flag.String("config-file", "config.json", "Config file to get twitter credentials")
+	flag.Parse()
+	return Opts{*configFile}
 }
